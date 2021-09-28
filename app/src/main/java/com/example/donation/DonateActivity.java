c63ba26a83d397c23083633d4b4a8b73365c974c
feelingBlue00 <pta230500x090800@gmail.com>
2021-09-29 03:09:03 +0700

    Add excercise 4

--- app/src/main/java/com/example/donation/DonateActivity.java
+++ app/src/main/java/com/example/donation/DonateActivity.java
@@ -4,14 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class DonateActivity extends AppCompatActivity {
     TextView textView;
     Button donateButton;
+    RadioGroup paymentMethod;
+    NumberPicker amountPicker;
+    ProgressBar progressBar;
+
+    private int totalDonated = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +31,19 @@ public class DonateActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.welcome);
         textView.setText(String.format("Welcome, %s", name));
 
+        paymentMethod = (RadioGroup) findViewById(R.id.paymentMethod);
+        amountPicker = (NumberPicker) findViewById(R.id.amountPicker);
+
+        amountPicker.setMinValue(0);
+        amountPicker.setMaxValue(1000);
+
         donateButton = (Button) findViewById(R.id.donateButton);
         if (donateButton != null) {
             Log.v("Donate", "Really got the donate button");
         }
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setMax(10000);
 
         donateButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +54,13 @@ public class DonateActivity extends AppCompatActivity {
     }
 
     public void donateButtonPressed() {
-        Log.v("Donate", "Donate Button Pressed");
+        int amount = amountPicker.getValue();
+        int radioId = paymentMethod.getCheckedRadioButtonId();
+        String method = radioId == R.id.PayPal ? "PayPal" : "Direct";
+        totalDonated = totalDonated + amount;
+        progressBar.setProgress(totalDonated);
+        Log.v("Donate", "Donate Button Pressed with amount " + amount);
+        Log.v("Donate", "Using method: " + method);
+        Log.v("Donate", "Current total: " + totalDonated);
     }
 }
