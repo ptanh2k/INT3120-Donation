53c99a7603f5d6ab04d5bbc03d909bce5ed60a28
feelingBlue00 <pta230500x090800@gmail.com>
2021-10-01 03:15:41 +0700

    Refactor: Modified donation report UI, fix some warnings

--- app/src/main/java/com/example/donation/Report.java
+++ app/src/main/java/com/example/donation/Report.java
@@ -1,27 +1,20 @@
 package com.example.donation;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
-public class Report extends AppCompatActivity {
+import adapters.DonationAdapter;
+
+public class Report extends Base {
     ListView listView;
-
-    //Array of data
-    static final String[] numbers = new String[] {
-            "Amount,  Pay method",
-            "10,      Direct",
-            "100,     PayPal",
-            "1000,    Direct",
-            "10,      PayPal",
-            "5000,    PayPal"
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +23,19 @@ public class Report extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.reportList);
 
+        // REPORT title becomes bold
+        TextView reportTitle = (TextView) findViewById(R.id.reportTitle);
+        reportTitle.setTypeface(null, Typeface.BOLD);
+
         //The Adapter acts as a bridge between the UI Component and the Data Source. It converts data from the data sources into view items that can be displayed into the UI Component.
         //Data Source can be Arrays, HashMap, Database, etc. and UI Components can be ListView, GridView, Spinner, etc
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, numbers);
+        DonationAdapter adapter = new DonationAdapter(this, donations);
         listView.setAdapter(adapter);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.general_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_donate, menu);
         return true;
     }
 
